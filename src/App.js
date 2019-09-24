dd3d3674c0c889533ad93bb71bebbb0c71d99754
Raphaelli Bellini <raphaelli.bellini@novatendencia.localdomain>
2019-09-24 10:31:31 -0300

    Add Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Main from './pages/Main'
+import Main from './pages/Main';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Provider store={store}>
+        <Main />
+      </Provider>
     </div>
   );
 }
