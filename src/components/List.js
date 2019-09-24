09b69cafdd163fcba37439928122112ab5562952
Raphaelli Bellini <raphaelli.bellini@novatendencia.localdomain>
2019-09-24 09:44:01 -0300

    Create the component 'List'

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <ul className='repo-list'>
+            <li>repo</li>
+            <li>repo</li>
+            <li>repo</li>
+        </ul>
+    )
+}
+
+export default List;
