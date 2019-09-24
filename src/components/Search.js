e0d2990c88aa68f44152e3595994783782acc14d
Raphaelli Bellini <raphaelli.bellini@novatendencia.localdomain>
2019-09-24 09:39:53 -0300

    Create the component 'Search'

---
+++ src/components/Search.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Grid, Segment, Input, Button } from 'semantic-ui-react';
+
+function Search() {
+    return (
+        <Grid className='search-component'>
+            <Grid.Row columns={1}>
+                <Grid.Column>
+                    <Segment inverted color='yellow' textAlign='center' className='error'>Não foi possível localizar o repositório solicitado.</Segment>
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={3} verticalAlign='middle' className='search-header'>
+                <Grid.Column width={2}>
+                    <img alt='Logo do Github' src="https://img.icons8.com/ios-glyphs/60/000000/github.png"></img>
+                </Grid.Column>
+                <Grid.Column width={12}>
+                    Repositórios
+                </Grid.Column>
+                <Grid.Column width={1} textAlign='right'>
+                    4
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={2}>
+                <Grid.Column width={12}>
+                    <Input
+                        focus
+                        className='search-bar'
+                        type='text'
+                        value={}
+                        onChange={}
+                    />
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <Button color='purple' className='search-button' onClick={}>ADD</Button>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    )
+
+}
+
+export default Search;
