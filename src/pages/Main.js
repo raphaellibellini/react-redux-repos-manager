db8cdb94511268f0f4d4395e99fdbc780864120b
Raphaelli Bellini <raphaelli.bellini@novatendencia.localdomain>
2019-09-24 09:35:22 -0300

    Structure the app into pages

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <span>Test</span>
+            </div>
+        )
+    }
+}
+
+export default Main;
